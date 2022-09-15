cdc0f4d76982ca827bcc295987a9b36965543627
so3 <theghost9921@gmail.com>
2022-09-15 08:02:21 +0700

    add entity/modules folder

---
+++ src/java/modules/AccountList.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modules;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AccountList {
+    
+}
