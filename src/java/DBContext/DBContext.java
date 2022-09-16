c61c6515f94a812e1cb0c9422728d6654e4eccea
so3 <theghost9921@gmail.com>
2022-09-16 07:43:46 +0700

    add DB connect

---
+++ src/java/DBContext/DBContext.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbcontext;
+
+/**
+ *
+ * @author LENOVO
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext {
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+        if(instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        }
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }   
+//      public String getImagePath() throws Exception {
+//        return "image/";
+//    }
+    /*Insert your other code right after this comment*/
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "LENOVO-ARH0528\\MSSQLSERVER01";
+    private final String dbName = "Account";
+    private final String portNumber = "1433";
+    private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "sa";
+    private final String password = "admin";
+    //Test Connect DB
+    public static void main(String[] args) {
+        try {
+            System.out.println(new DBContext().getConnection());
+            
+        } catch (Exception e) {
+        }
+     
+    }
+
+    
+}
+ 
