016400533bd0c6cd431c0888f87083c53036ff02
so3 <theghost9921@gmail.com>
2022-09-18 19:33:49 +0700

    add AccountDAO

---
+++ src/java/DAO/AccountDAO.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import DBContext.DBContext;
+import entity.Account;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AccountDAO implements Serializable {
+    public ArrayList<Account> getListAccounts() throws SQLException {
+        ArrayList<Account> accounts = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            String sql = "select * from account order by account_id";
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("account_id");
+                String name = rs.getString("name").trim();
+                String phone = rs.getString("phone").trim();
+                String email = rs.getString("email").trim();
+                String username = rs.getString("username").trim();
+                String password = rs.getString("password").trim();
+                int role_id = rs.getInt("role_id");
+
+                accounts.add(new Account(id, name, phone, email, username, password, role_id));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            ps.close();
+            conn.close();
+        }
+        return accounts;
+    }
+
+    public void createAccount(Account account) throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        //ResultSet rs = null;
+        try {
+            String sql = "insert into account values (?, ? , ?, ?, ?, ?, ?) ";
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, account.getAccount_id());
+            ps.setString(2, account.getName());
+            ps.setString(3, account.getPhone());
+            ps.setString(4, account.getEmail());
+            ps.setString(5, account.getUsername());
+            ps.setString(6, account.getPassword());
+            ps.setInt(7, account.getRole_id());
+
+            ps.execute();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            ps.close();
+            ps.close();
+        }
+    }
+
+    public void deleteAccount(int id) throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        //ResultSet rs = null;
+        String sql = "delete from account where account_id =?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ps.close();
+            conn.close();
+        }
+    }
+
+    public void updateStudent(Account account) throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        //ResultSet rs = null;
+        
+        try {
+            String sql = "update account set name=?, phone=?, email=?, username=?, password=?, role_id=? where account_id=?";
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(7, account.getAccount_id());
+            ps.setString(1, account.getName());
+            ps.setString(2, account.getPhone());
+            ps.setString(3, account.getEmail());
+            ps.setString(4, account.getUsername());
+            ps.setString(5, account.getPassword());
+            ps.setInt(6, account.getRole_id());
+            ps.execute();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            ps.close();
+            ps.close();
+        }
+    }
+}
