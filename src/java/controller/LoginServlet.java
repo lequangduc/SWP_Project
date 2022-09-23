a2702eda1d82e889876816d80383580e6bb495f9
so3 <theghost9921@gmail.com>
2022-09-23 08:09:28 +0700

    add LoginServlet

---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import entity.Account;
+import model.AccountList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //get parameters from jsp
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        
+        AccountList list = new AccountList();
+        ArrayList<Account> accounts = list.accountsgetAccountList();;
+        
+        
+        /// test account
+        if(userName.equals("admin") && password.equals("admin")){
+            request.getRequestDispatcher("AdminPage.jsp").forward(request, response);return;}
+        ///
+        if(userName.isEmpty() || password.isEmpty()) {
+            request.setAttribute("LoginError","User name or Password is bank");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        }
+        for(int i = 0; i < accounts.size(); i++) {
+            if(accounts.get(i).getUsername().equals(userName) && accounts.get(i).getPassword().equals(password)){
+                HttpSession session= request.getSession();
+                session.setAttribute("userLogin", accounts.get(i));
+                session.setAttribute("userRegister",accounts.get(i));
+                request.getRequestDispatcher("home-page.jsp").forward(request, response);
+                break;
+            }
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
